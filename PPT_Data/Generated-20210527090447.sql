c2aba386b07ffda8cbe2c057ee38327e69db2769
Tan Vo <vht1092@gmail.com>
2021-05-27 09:25:46 +0700

    Add files via upload

---
+++ PPT_Data/Generated-20210527090447.sql
@@ -0,0 +1,12 @@
+--------------------------------------------------------
+--  File created - Thursday-May-27-2021   
+--------------------------------------------------------
+@D:\Users\tanvh1\PPT_SYS_PERMISSION_DATA_TABLE.sql
+@D:\Users\tanvh1\PPT_SENDMAIL_DATA_TABLE.sql
+@D:\Users\tanvh1\PPT_SYS_GROUP_DATA_TABLE.sql
+@D:\Users\tanvh1\PPT_SYS_USER_DATA_TABLE.sql
+@D:\Users\tanvh1\PPT_SYS_VIEW_DATA_TABLE.sql
+@D:\Users\tanvh1\PPT_SENTMAIL_DATA_TABLE.sql
+@D:\Users\tanvh1\PPT_SYS_USR_BRANCH_DATA_TABLE.sql
+@D:\Users\tanvh1\PPT_CRD_PGM_DATA_TABLE.sql
+@D:\Users\tanvh1\PPT_HIBERNATE_SEQUENCES_DATA_TABLE.sql
